63f91d4ab726f476b803ee6a9462174b98af07c8
Татьяна Анисимова <kulmina@mail.ru>
2021-11-19 20:43:32 +0300

    Добавьте файлы проекта.

---
+++ Lab5_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Введите число=");
+            N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = 1 - (i + j) % 2; //проверка на четность, ищем остаток от деления на 2, Если число четное, результатом будет 0, если нечетное – 1.
+                    Console.Write(array[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab5_HomeW/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("Введите числа");
+            array[0] = Convert.ToInt32(Console.ReadLine());
+            array[1] = Convert.ToInt32(Console.ReadLine());
+            array[2] = Convert.ToInt32(Console.ReadLine());
+            array[3] = Convert.ToInt32(Console.ReadLine());
+            array[4] = Convert.ToInt32(Console.ReadLine());
+            array[5] = Convert.ToInt32(Console.ReadLine());
+            array[6] = Convert.ToInt32(Console.ReadLine());
+            {
+                float S = 0;
+                for (int i = 0; i < 7; i++)
+                {
+                    S += array[i];
+                    Console.Write("{0} ", array[i]);
+                }
+
+                Console.Write("{0,7:f2} ", S / 7);
+            }
+            Console.ReadKey();
+        }
+    }
+}
